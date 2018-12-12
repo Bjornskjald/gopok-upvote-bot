f97a85d7a0484c86d5d7520517e1d5630188d6f7
Bjornskjald <github@bjorn.ml>
2018-12-12 22:58:07 +0100

    Added Grease/Tampermonkey header

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// ==UserScript==
+// @name         Gopok Upvote Bot
+// @version      1.0.0
+// @description  upvote all the posts
+// @author       Bjornskjald
+// @match        https://gopok.io
+// @grant        none
+// ==/UserScript==
+
 document.querySelectorAll('.v-btn.v-btn--floating.v-btn--outline.theme--light.blue--text').forEach(button => button.click())
